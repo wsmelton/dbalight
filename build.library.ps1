8a108e4e05a379c16abcca7395926cc47661446f
Shawn Melton <smelton@loandepot.com>
2023-02-18 15:16:43 -0600

    update library build

--- build.library.ps1
+++ build.library.ps1
@@ -6,35 +6,38 @@ param(
 )
 
 $script:ModuleManifest = [IO.Path]::Combine($PSScriptRoot,'src','dbalight.psd1')
+$script:ModuleTypes = [IO.Path]::Combine($PSScriptRoot,'src','dbalight.types.ps1xml')
+$script:ModuleFormats = [IO.Path]::Combine($PSScriptRoot,'src','dbalight.format.ps1xml')
 $script:ProjectRoot = [IO.Path]::Combine($PSScriptRoot,'src','dbalight')
 $script:ProjectFile = [IO.Path]::Combine($ProjectRoot, 'dbalight.csproj')
-$script:ProjectOut = [IO.Path]::Combine($PSScriptRoot,'src','bin')
+$script:ProjectOut = [IO.Path]::Combine($PSScriptRoot,'src','output')
 $script:ProjectLibFile = [IO.Path]::Combine($ProjectOut,'dbalight.dll')
 $script:Manifest = Import-PowerShellDataFile -Path $ModuleManifest
 $script:Version = $Manifest.ModuleVersion
 
 # Ensure and call the module.
 if ($MyInvocation.ScriptName -notlike '*Invoke-Build.ps1') {
-    $InvokeBuildVersion = '5.7.3'
+    $InvokeBuildVersion = '5.10'
     $ErrorActionPreference = 'Stop'
     try {
-        Import-Module InvokeBuild -RequiredVersion $InvokeBuildVersion
+        Import-Module InvokeBuild -MinimumVersion $InvokeBuildVersion
     } catch {
-        Install-Module InvokeBuild -RequiredVersion $InvokeBuildVersion -Scope AllUsers -Force
-        Import-Module InvokeBuild -RequiredVersion $InvokeBuildVersion
+        Install-Module InvokeBuild -MinimumVersion $InvokeBuildVersion -Scope AllUsers -Force
+        Import-Module InvokeBuild -MinimumVersion $InvokeBuildVersion
     }
     Invoke-Build -Task $Tasks -File $MyInvocation.MyCommand.Path @PSBoundParameters
     return
 }
 
 task Clean -Before Build {
-    if (Test-Path $ProjectLibFile) {
-        Remove-Item $ProjectLibFile
+    if (Test-Path $ProjectOut) {
+        Remove-Item $ProjectOut -Recurse -Force
     }
 }
 
 task Build {
     exec { dotnet build --configuration $Configuration --output $ProjectOut /p:Version=$Version $ProjectFile }
+    Get-ChildItem $ModuleManifest,$ModuleTypes,$ModuleFormats | Copy-Item -Destination $ProjectOut
 }
 
 task . build
--- src/dbalight.psd1
+++ src/dbalight.psd1
@@ -2,13 +2,13 @@
     ModuleVersion        = '0.1.0'
     Description          = '🔦 into your SQL Server to find, test, troubleshoot or report on your environment.'
     CompatiblePSEditions = 'Desktop', 'Core'
-    RootModule           = 'dbalight.psm1'
     TypesToProcess       = 'dbalight.types.ps1xml'
     FormatsToProcess     = 'dbalight.format.ps1xml'
+    NestedModules        = @('dbalight.dll')
     GUID                 = '527d50d0-c8f3-4947-a204-27f343a39da7'
     Author               = 'Shawn Melton'
     CompanyName          = 'Shawn Melton'
-    Copyright            = '(c) 2021. All rights reserved.'
+    Copyright            = '(c) 2023. All rights reserved.'
     PowerShellVersion    = '7.1'
     PrivateData          = @{
         PSData = @{
