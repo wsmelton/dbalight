98bb5941525b6c38918b765da312dae8e2109a6c
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2023-10-08 00:37:09 -0500

    ModuleBuilder bootstrap script, remove older InvokeBuild script

--- build.ps1
+++ build.ps1
@@ -1,143 +1,51 @@
-#requires -Module InvokeBuild
-[cmdletbinding()]
+#requires -Version 7.2
+#requires -Modules ModuleBuilder
+<#
+    .SYNOPSIS
+    Script to build the dbalight solution and module
+#>
+[CmdletBinding()]
 param(
-    [Parameter(ParameterSetName = 'docs')]
-    [switch]
-    $PublishDocs,
-
-    [Parameter(ParameterSetName = 'publish')]
+    # A specific folder to build into, defaults to workspace root
     [string]
-    $GalleryKey,
+    $OutputDirectory = "Output",
 
-    [Parameter(ParameterSetName = 'publish')]
-    [switch]
-    $Prerelease,
-
-    [Parameter(ParameterSetName = 'release')]
-    [Parameter(ParameterSetName = 'publish')]
-    [switch]
-    $Release,
-
-    [Parameter(ParameterSetName = 'release')]
-    [Parameter(ParameterSetName = 'publish')]
-    [switch]
-    $Draft,
-
-    [Parameter(ParameterSetName = 'publish')]
-    [switch]
-    $SkipTests
+    # The version of the output module
+    [Alias("ModuleVersion")]
+    [string]
+    $SemVer
 )
-Push-Location
-Set-Location $PSScriptRoot
-$moduleName = 'dbalight'
-$staging = "$env:TEMP\dbalight_staging\"
 
-$git = git status
-if ($git[1] -notmatch "Your branch is up to date" -and (-not $PSBoundParameters.ContainsKey('PublishDocs'))) {
-    Pop-Location
-    throw "Local branch is dirty"
+$ErrorView = 'DetailedView'
+$ErrorActionPreference = "Stop"
+Push-Location $PSScriptRoot -StackName builderscript
+$OutputDirectory = Join-Path $Pwd $OutputDirectory
+
+$dllFileName = 'dbalight.dll'
+if (-not (Get-Command dotnet -CommandType Application -ErrorAction Ignore)) {
+    throw "dotnet not found installed or in your path"
 }
 
-if (Test-Path $staging) {
-    Remove-Item -Recurse -Force $staging
-}
-
-if (Get-Module dbalight) {
-    Remove-Module dbalight -Force
-}
-
-$imported = Import-Module "$PSScriptRoot\src\output\dbalight.psd1" -Force -PassThru
-
-if ($PSBoundParameters['PublishDocs']) {
-    if ($PSEdition -eq 'Desktop') {
-        Write-Warning "Doc processing has to run under PowerShell Core"
-        return
+if (-not $SemVer) {
+    if ($SemVer = dotnet gitversion -showvariable SemVer) {
+        $null = $PSBoundParameters.Add("SemVer", $SemVer)
     }
-    $docCommandPath = "$PSScriptRoot\docs\"
-    Write-Host "Removing old command docs [$docCommandPath]" -ForegroundColor Black -BackgroundColor DarkCyan
-    Remove-Item $docCommandPath -Filter *.md -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue -Verbose
-
-    Import-Module platyPS
-    $cmdParams = @{
-        Module      = $moduleName
-        CommandType = 'Cmdlet'
-    }
-    $commands = Get-Command @cmdParams
-
-    Write-Host "Generating new command docs [$docCommandPath]" -ForegroundColor Black -BackgroundColor DarkCyan
-    foreach ($cmd in $commands) {
-        switch ($cmd.Name) {
-            { $_ -match 'Db' } { $category = 'database' }
-            { $_ -match 'Instance' } { $category = 'instance' }
-            default { $category = 'general' }
-        }
-        $metadata = @{
-            'category' = $category
-            'title'    = $cmd.Name
-        }
-
-        New-MarkdownHelp -OutputFolder $docCommandPath -Command $cmd.Name -Metadata $metadata -Force
-    }
-    return
 }
 
-if (-not $PSBoundParameters['SkipTests']) {
-    Import-Module Pester
-    $tests = Invoke-Pester -Path "$PSScriptRoot\tests" -Output Minimal -PassThru
-}
+<# clean it before we build it #>
+dotnet clean -c release
 
-if ($PSBoundParameters['Prerelease']) {
-    $foundModule = Find-Module -Name $moduleName -AllowPrerelease:$Prerelease -Repository PSGallery
+<# now we build #>
+$VersionNuget = dotnet gitversion -showvariable NuGetVersion -config ./GitVersion.yml
+$VersionPrefix, $VersionSuffix = $VersionNuget -split '[-+]', 2
+dotnet build -c release -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix
+
+<# Find the DLL #>
+$dll = Get-ChildItem -Recurse -File -Filter $dllFileName | Select-Object -First 1
+if ($dll) {
+    Write-Host "dotnet build successful, found $($dll.FullName)"
+    Write-Host " - Build $($Module.Name) $SemVer" -ForegroundColor Cyan
+    Build-Module -SourcePath ./source -Target CleanBuild -OutputDirectory $OutputDirectory
 } else {
-    $foundModule = Find-Module -Name $moduleName -Repository PSGallery
+    Write-Warning "No build occurred, unable to find DLL library: $dllFileName"
 }
-
-if ($foundModule.Version -ge $imported.Version) {
-    Write-Warning "PowerShell Gallery version of $moduleName is more recent ($($foundModule.Version) >= $($imported.Version))"
-}
-
-if ($tests.FailedCount -eq 0 -or $PSBoundParameters['SkipTests']) {
-    $moduleTempPath = Join-Path $staging $moduleName
-    Write-Host "Staging directory: $moduleTempPath"
-    $imported | Split-Path | Copy-Item -Destination $moduleTempPath -Recurse
-
-    if ($PSBoundParameters['GalleryKey']) {
-        try {
-            Write-Host "Publishing $moduleName [$($imported.Version)] to PowerShell Gallery"
-
-            Publish-Module -Path $moduleTempPath -NuGetApiKey $gallerykey -Repository PSGallery
-            Write-Host "successfully published to PS Gallery"
-        } catch {
-            Write-Warning "Publish failed: $_"
-        }
-    }
-    if ($PSBoundParameters['Release']) {
-        if ((gh config get prompt) -eq 'enabled') {
-            Invoke-Expression "gh config set prompt disabled"
-        }
-        $moduleData = Import-PowerShellDataFile "$staging\$moduleName\$moduleName.psd1"
-        $changeLog = [IO.Path]::Combine([string]$PSScriptRoot, 'release.md')
-        Compress-Archive "$staging\$moduleName\*" -DestinationPath "$staging\$moduleName.zip" -CompressionLevel Fastest -Force
-        $ghArgs = "release create `"v$($moduleData.ModuleVersion)`" `"$($staging)$($moduleName).zip#$($moduleName).zip`" --title `"dbalight $($moduleData.ModuleVersion)`" --notes-file $changeLog"
-        if ($PSBoundParameters['Prerelease']) {
-            $ghArgs = $ghArgs + " --prerelease"
-        }
-        if ($PSBoundParameters['Draft']) {
-            $ghArgs = $ghArgs + " --draft"
-        }
-
-        Write-Host "gh command to execute: $ghArgs" -ForegroundColor DarkYellow
-
-        Invoke-Expression "gh $ghArgs"
-
-        if ((gh config get prompt) -eq 'disabled') {
-            Invoke-Expression "gh config set prompt enabled"
-        }
-    }
-
-    Remove-Item -Recurse -Force $staging
-} else {
-    Remove-Item -Recurse -Force $staging
-    Write-Host "Tests failures detected; cancelling and cleaning up"
-}
-Pop-Location
